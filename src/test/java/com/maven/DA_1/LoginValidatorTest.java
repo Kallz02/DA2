b3f2a57288ef5a8f632061b60347dc30cb52e37e
Kallz02 <akshay.pranav.kalathil@gmail.com>
2023-10-30 21:16:49 +0530

    pom.xml error

--- src/test/java/com/maven/DA_1/LoginValidatorTest.java
+++ src/test/java/com/maven/DA_1/LoginValidatorTest.java
@@ -9,22 +9,24 @@ public class LoginValidatorTest {
     @Test
     public void testValidCredentials() {
         LoginValidator loginValidator = new LoginValidator();
-        
-        System.out.println("[TEST CASE 1]: Testing with the correct username and password !!");
-        
-        boolean result = loginValidator.validateCredentials("AK", "20MIC0071");
-        
+
+        System.out.println(
+                "[TEST CASE 1]: Testing with the correct username and password !!");
+
+        boolean result = loginValidator.validateCredentials("AK", "20MIC0023");
+
         Assert.assertEquals(result, true, "Expected TRUE but got FALSE");
     }
 
     @Test
     public void testInvalidCredentials() {
         LoginValidator loginValidator = new LoginValidator();
-        
-        System.out.println("[TEST CASE 2]: Testing with an incorrect username and password combination !!");
-        
+
+        System.out.println(
+                "[TEST CASE 2]: Testing with an incorrect username and password combination !!");
+
         boolean result = loginValidator.validateCredentials("wrong_username", "wrong_password");
-        
+
         Assert.assertEquals(result, false, "Expected FALSE but got TRUE");
     }
 }
